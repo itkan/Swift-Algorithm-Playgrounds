b34763d66d2523c971a6f43b451318a2f2a38810
Ankit <gupta.ankit.vi@gmail.com>
2016-09-20 00:14:40 +0530

    test cases updated

--- bubbleSort.playground/Contents.swift
+++ bubbleSort.playground/Contents.swift
@@ -25,8 +25,26 @@ public func bubbleSort(array:Array<Int>) -> Array<Int> {
     return tArray
 }
 
-//Test Sorting
-let input = [6,4,5,8,2,3,1,0,9,7]
-let target = [0,1,2,3,4,5,6,7,8,9]
-let result = bubbleSort(array: input)
-XCTAssertEqual(result, target)
+//Test Average case Sorting
+let input1 = [6,4,5,8,2,3,1,0,9,7]
+let target1 = [0,1,2,3,4,5,6,7,8,9]
+let result1 = bubbleSort(array: input1)
+XCTAssertEqual(result1, target1)
+
+//Test one element
+let input2 = [0]
+let target2 = [0]
+let result2 = bubbleSort(array: input2)
+XCTAssertEqual(result2, target2)
+
+//Test two element
+let input3 = [1,0]
+let target3 = [0,1]
+let result3 = bubbleSort(array: input3)
+XCTAssertEqual(result3, target3)
+
+//Test Worst case
+let input4 = [9,8,7,6,5,4,3,2,1,0]
+let target4 = [0,1,2,3,4,5,6,7,8,9]
+let result4 = bubbleSort(array: input4)
+XCTAssertEqual(result4, target4)
