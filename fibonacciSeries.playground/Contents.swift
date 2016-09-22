bf1d9c0f9865af9a29d2a2f5edcbf80cfb6aca56
Ankit <gupta.ankit.vi@gmail.com>
2016-09-22 19:30:46 +0530

    optimized Fibonacci

--- fibonacciSeries.playground/Contents.swift
+++ fibonacciSeries.playground/Contents.swift
@@ -6,46 +6,36 @@
  Fn = Fn-1 + Fn-2
  
  The initial values of F0 & F1 to be taken as 0, 1
- 
- Check: Loop is more efficient than recursion in case of Fibonacci
 */
 
 public class Fibonacci {
-    public class func recursive(n:Int) {
-        for i in 0..<n {
-            print(Fibonacci.calculate(n: i))
-        }
-    }
-    
-    public class func calculate(n:Int) -> Int {
-        if n == 0 {
-            return 0
-        } else if n == 1 {
-            return 1
-        }else {
-            return (calculate(n: n-1) + calculate(n: n-2))
-        }
-    }
-    
     public class func loop(n:Int) {
         var first = 0, next = 1
         
         for i in 0..<n {
             if i < 2 {
-                print(i)
+                print(i," ", terminator:"")
             }else {
                 let result = first + next
                 first = next
                 next = result
-                print(result)
+                print(result," ", terminator:"")
             }
+        }
+    }
+    
+    public class func recursive(sum:Int,last:Int,n:Int) {
+        print(last," ", terminator:"")
+        let newSum = sum + last
+        if (n >= 2) {
+            recursive(sum: newSum, last: sum, n: n-1)
         }
     }
 }
 
 print("Recursion")
-Fibonacci.recursive(n: 10)
+Fibonacci.recursive(sum: 1, last: 0, n: 10)
 print()
 print()
-print("Loop: Efficient")
+print("Loop")
 Fibonacci.loop(n: 10)
